a35d668cb3efb5036bae484865eaf8eacab6eeb8
Rob Starling <70387748+robertstarling@users.noreply.github.com>
2025-09-11 23:39:48 +0000

    use hostname in public key

--- setup-common.sh
+++ setup-common.sh
@@ -3,14 +3,16 @@
 # This file is fully idempotent, so feel free to run it multiple times.
 
 create_github_ssh_key_if_missing() {
-  if [ ! -f "$HOME/.ssh/id_ed25519_github" ]; then
-    ssh-keygen -t ed25519 -C "rob@nc-devbox-2509" -f "$HOME/.ssh/id_ed25519_github"
-    ssh-add "$HOME/.ssh/id_ed25519_github"
-    echo "Add public key to GitHub → Settings → SSH and GPG keys → New SSH key:"
-    cat  "$HOME/.ssh/id_ed25519_github.pub"
-    echo 
-    echo "Then confirm access with: ssh -T git@github.com"
+  if [ -f "$HOME/.ssh/id_ed25519_github" ]; then
+    echo "GitHub SSH key already exists, skipping."
+    return
   fi
+  ssh-keygen -t ed25519 -C "rob@$(hostname)" -f "$HOME/.ssh/id_ed25519_github"
+  ssh-add "$HOME/.ssh/id_ed25519_github"
+  echo "Add public key to GitHub → Settings → SSH and GPG keys → New SSH key:"
+  cat  "$HOME/.ssh/id_ed25519_github.pub"
+  echo 
+  echo "Then confirm access with: ssh -T git@github.com"
 }
 
 main() {
