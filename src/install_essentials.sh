34e01386f973c4b413d59074038252dae04b73d8
Rob Starling <70387748+robertstarling@users.noreply.github.com>
2026-03-25 05:28:32 +0000

    squash for re-base

--- src/install_essentials.sh
+++ src/install_essentials.sh
@@ -3,29 +3,30 @@
 # Make sure we're getting the latest version of git
 # sudo add-apt-repository ppa:git-core/ppa -y
 
-sudo apt-get -y -q update
+# sudo apt-get -y -q update
 
 # The following are library-type things. They're basically dependencies
 # sudo apt-get install -y -q \
-  # libncurses-dev \
-  # libgtk2.0-dev \
-  # libatk1.0-dev \
-  # libcairo2-dev \
-  # libx11-dev \
-  # libxpm-dev \
-  # libxt-dev \
-  # python3-dev \
-  # python3-venv \
-  # python3.10-venv \
-  # ruby-dev \
-  # lua5.1 \
-  # liblua5.1-0-dev \
-  # libperl-dev \
-  # ;
+#   libncurses-dev \
+#   libgtk2.0-dev \
+#   libatk1.0-dev \
+#   libcairo2-dev \
+#   libx11-dev \
+#   libxpm-dev \
+#   libxt-dev \
+#   python3-dev \
+#   python3-venv \
+#   python3.10-venv \
+#   ruby-dev \
+#   lua5.1 \
+#   liblua5.1-0-dev \
+#   libperl-dev \
+#   ;
 
 # These are my programs
 sudo apt-get install -y -q \
   make \
+  pinentry-tty \
   ;
   # curl \
   # git \
